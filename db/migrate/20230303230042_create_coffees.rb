003c4b988cd8c6d2097a2d51df642e24e5d19c4e
XiWang <vivian.xi.wang@gmail.com>
2023-03-03 15:08:18 -0800

    migrate coffees table

--- db/migrate/20230303230042_create_coffees.rb
+++ db/migrate/20230303230042_create_coffees.rb
@@ -1,10 +1,10 @@
 class CreateCoffees < ActiveRecord::Migration[7.0]
   def change
     create_table :coffees do |t|
-      t.string :name
-      t.integer :year
-      t.float :caffine_content
-      t.float :caffine_percentage
+      t.string :name, null: false
+      t.integer :year, null: false
+      t.float :caffine_content, null:false
+      t.float :caffine_percentage,null: false
 
       t.timestamps
     end
