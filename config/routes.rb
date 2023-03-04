c11ab4bd4cd7d595cacb27139b83cdb88d78bb40
XiWang <vivian.xi.wang@gmail.com>
2023-03-03 18:54:19 -0800

    customized routes for coffee and post

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,13 @@ Rails.application.routes.draw do
   get '/coffee/:id', to: 'coffee#show'
   post '/coffee/create', to: 'coffee#create'
   delete '/coffee/delete/:id', to: 'coffee#destroy'
+
+
+  get '/post/ping', to: 'post#ping'
+  get '/post', to: 'post#index'
+  get  '/post/:id', to: 'post#show'
+  post '/post', to: 'post#create'
+  delete '/post/:id', to: 'post#destroy'
+
+  get '/post/coffee', to: 'post#sort'
 end
