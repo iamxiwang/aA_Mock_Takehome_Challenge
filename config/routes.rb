7003d2f2b07672af8fee4e9f255f3a4df4a6c509
XiWang <vivian.xi.wang@gmail.com>
2023-03-03 17:49:11 -0800

    added coffee routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get '/coffee/ping', to: 'coffee#ping'
+  get '/coffee', to:'coffee#index'
+  get '/coffee/:id', to: 'coffee#show'
+  post '/coffee/create', to: 'coffee#create'
+  delete '/coffee/delete/:id', to: 'coffee#destroy'
 end
