ded5f20626a68acb56752ffd30141417ea8a2e3d
XiWang <vivian.xi.wang@gmail.com>
2023-03-04 14:56:49 -0800

    added ping action in controller

--- app/controllers/coffees_controller.rb
+++ app/controllers/coffees_controller.rb
@@ -1,5 +1,8 @@
 class CoffeesController < ApplicationController
-
+    def ping
+        render json: status: 'good'
+    end
+    
     def index
         @coffees = Coffee.order('params[:order]').all
         render :index
