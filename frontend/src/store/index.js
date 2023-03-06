0fa1a517c991b1537001bfb13d7e99be71fbd7f2
XiWang <vivian.xi.wang@gmail.com>
2023-03-05 17:05:35 -0800

    finished index in store

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -6,3 +6,19 @@ const rootReducer = combineReducer({
     posts: postsReducer,
     coffees: coffeesReducer
 })
+
+let enhancer;
+
+if(ProcessingInstruction.env.NODE_ENV === 'porduction'){
+    enhancer = applyMiddleware(thunk);
+}else{
+    const logger = require('redux-logger').default;
+    const composeEnhancers =
+        window._REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+    enhancer = composeEnhancers(applyMiddleware(thunk, logger))
+}
+const configureStore = (preloadedState) => {
+    return createStore(rootReducer, preloadedState, enhancer);
+};
+
+export default configureStore
